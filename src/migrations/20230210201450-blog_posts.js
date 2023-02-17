121de1bac8bb061ed608f5d74a66e39a5c3c927a
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-02-17 09:54:46 -0300

    req 17 - alteracoes na migrate

--- src/migrations/20230210201450-blog_posts.js
+++ src/migrations/20230210201450-blog_posts.js
@@ -1,45 +1,47 @@
 'use strict';
 
-  module.exports = {
-    up: async (queryInterface, Sequelize) => {
-      await queryInterface.createTable('blog_posts', {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'title',
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        field: 'user_id',
+        references: {
+          model: 'users',
+          key: 'id',
         },
-        title: {
-          allowNull: false,
-          type: Sequelize.STRING,
-          field: 'title',
-        },
-        content: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        user_id: {
-          allowNull: false,
-          type: Sequelize.INTEGER,
-          references: {
-            model: 'users', // model esta referenciando o nome da tabela
-            key: 'id',
-          },
-        },
-        published: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          // field: 'published',
-        },
-        updated: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          field: 'updated',
-        },
-      });
-    },
-  
-    down: async (queryInterface, Sequelize) => {
-      await queryInterface.dropTable('blog_posts');
-    }
-  };
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        // field: 'published',
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
